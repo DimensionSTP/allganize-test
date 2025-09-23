6e12dcfa0cb0fb1b4d7906930af83416fac70179
DimensionSTP <ddang8jh@gmail.com>
2025-09-24 08:05:22 +0900

    delete: unused modules, scripts

--- scripts/ui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-remote_mode=True
-
-streamlit run app.py \
-    remote_mode=$remote_mode
--- src/apis/__init__.py
+++
@@ -1,7 +0,0 @@
-from .recommend import recommend_api
-from .report import report_api
-
-__all__ = [
-    "recommend_api",
-    "report_api",
-]
